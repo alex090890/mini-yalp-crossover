c592275cfa523dc3ed5404b455fdcd161f9a3eaf
Maria Jose Molina <marymolina1301@gmail.com>
2024-03-02 12:14:11 +0100

    seachbar

--- mini-yelp/src/App.jsx
+++ mini-yelp/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Header from './components/Header'
 
+
+
 function App() {
 
 
--- mini-yelp/src/components/Header.jsx
+++ mini-yelp/src/components/Header.jsx
@@ -1,6 +1,9 @@
+import SearchBar from "./SeachBar";
+
 export default function Header() {
     return (
         <div className="header">
+            <SearchBar/>
 
         </div>
     )
