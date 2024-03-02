bdd49cbbc4e05cf869f72302f4de6a526025394c
OLEKSANDR LAZURENKO <alexprofteach@gmail.com>
2024-03-02 14:54:01 +0000

    Worked on the menu

--- mini-yelp/src/components/Header.jsx
+++ mini-yelp/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import SearchBar from "./searchbar/SeachBar";
 import './Header.css';
 import MainMenu from './MainMenu'
-import { Box, Flex, Heading, Button} from '@chakra-ui/react';
+import { Flex, Button} from '@chakra-ui/react';
 import { Select } from '@chakra-ui/react'
 
 
@@ -10,9 +10,8 @@ export default function Header() {
         <>
         <div className="header">
             <SearchBar/>
-            <Flex alignItems="center" p={4} bg="white">
             <Flex alignItems="center" p={4} bg="white" justifyContent="flex-start">
-      <Select placeholder='Yelp For Business' _hover={{ bg: 'gray.200', cursor: 'pointer'}} color="white" mr={30} bg="transparent">
+      <Select placeholder='Yelp For Business' _hover={{ bg: 'gray.200', cursor: 'pointer'}} color="black" mr={30} bg="transparent">
         <option value='option1'>Login into Business Account</option>
         <option value='option2'>Claim Your Business</option>
         <option value='option3'>Explore Yelp For Business</option>
@@ -20,7 +19,8 @@ export default function Header() {
       <Button colorScheme='blue' _hover={{ bg: 'blue.600', cursor: "pointer" }} mr={10}>Write an Article </Button>
       <Button colorScheme='blue' _hover={{ bg: 'blue.600' }} mr={10} color="Blue">Login</Button>
       <Button colorScheme='blue' _hover={{ bg: 'red.500' }} mr={10} color="Blue">Register</Button>
-    </Flex>    
+          </Flex>
+           <MainMenu />      
         </div>
         </>
         
