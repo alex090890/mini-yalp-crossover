0be8463c75b9958af7ef178a4c5e996284f2843e
OLEKSANDR LAZURENKO <alexprofteach@gmail.com>
2024-03-02 17:39:45 +0000

    Final changes

--- mini-yelp/src/App.jsx
+++ mini-yelp/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import RestaurantsList from './components/RestaurantsList'
 import Hero from './components/Hero'
+import Footer from './components/Footer'
 
 
 
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <Hero />
       <RestaurantsList />
+      <Footer />
     </>
   )
 }
--- mini-yelp/src/components/Footer.jsx
+++ mini-yelp/src/components/Footer.jsx
@@ -1,18 +1,67 @@
-import {
-    FormControl,
-    Select,Grid,GridItem
-  } from '@chakra-ui/react'
+import { Box, Flex, Spacer } from '@chakra-ui/react';
+
+import './Footer.css'
 
   const Footer = () => {
     return ( 
         <>
-        
-  
-        
-       
+        <div className='divFooter'>
+  <div className='footerContainer'>
+ 
+
+  <Flex>
+    <Box w='150px' h='10' >
+      <h3>About</h3>
+      <a href="#About Yelp">About Yelp <br/>  </a> 
+      <a href="#Jobs">Jobs <br/> Press</a> 
+      <a href="#rust & Safety">rust & Safety<br/> </a> 
+      <a href="#Content guidelines">Content guidelines<br/> </a> 
+      <a href="#Terms of use & imprint">Terms of use & imprint <br/> </a> 
+      <a href="#Privacy policy">Privacy policy <br/> </a> 
+      <a href="#AdChoices">AdChoices <br/> </a> 
+      <a href="#Manage cookies">Manage cookies</a> 
+    </Box>
+    <Spacer />
+    <Box w='170px' h='10' >
+      <h3>Discover</h3>
+      <a href="#Collections">Collections <br/> </a> 
+      <a href="#Forum">Forum <br/> </a> 
+      <a href="#Events">Events <br/> </a> 
+      <a href="#rust & Safety">Help <br/> </a> 
+      <a href="#Yelp on'm mobile phone">Yelp on the mobile phone  <br/></a> 
+      <a href="#Developers">Developers <br/></a> 
+      <a href="#RSS">RSS<br/></a> 
+      <a href="#Manage cookies">Manage cookies <br/></a> 
+    </Box>
+    <Spacer />
+    <Box w='180px' h='10' >
+      <h3>Yelp for Business</h3>
+      <a href="#Yelp for Business">Yelp for Business <br/> </a> 
+      <a href="#Business Owner Login">Business Owner Login <br/> </a> 
+      <a href="#Apply your Yelp entry">Apply your Yelp entry <br/> </a> 
+      <a href="#Advertising on Yelp">Advertising on Yelp <br/> </a> 
+      <a href="#THelp for business owners">Help for business owners  <br/></a> 
+      <a href="#Yelp Blog for Business">Yelp Blog for Business <br/></a>
+    </Box>
+    <Spacer />
+    <Box w='180px' h='10'>
+      <h3>Languages</h3>
+      <a href="#English">English <br/> </a> 
+      <h3>Countries</h3>
+      <a href="#Germay">Germany <br/> </a> 
+    </Box>
+
+  </Flex>
+
+
+
+  </div>
+
+  <p className='copyright'>Copyright {new Date().getFullYear()}- {new Date().getFullYear()} Yelp </p>
+</div>
 
         </>
      );
   }
-   
+
   export default Footer;
