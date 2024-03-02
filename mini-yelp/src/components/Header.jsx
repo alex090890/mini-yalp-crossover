c1b75b4491b5e1d7baf322164faa18cef212ddf7
jnepmark <123545579+jnepmark@users.noreply.github.com>
2024-03-02 15:33:30 +0100

    comt

--- mini-yelp/src/components/Header.jsx
+++ mini-yelp/src/components/Header.jsx
@@ -10,16 +10,16 @@ export default function Header() {
         <>
         <div className="header">
             <h1>Hello There</h1>
-            <Flex alignItems="center" p={4} bg="white">
-      <Select placeholder='Yelp For Business' _hover={{ bg: 'gray.200', cursor: 'pointer'}} color="white" ml={3} bg="transparent">
+            <Flex alignItems="center" p={4} bg="white" justifyContent="flex-start">
+      <Select placeholder='Yelp For Business' _hover={{ bg: 'gray.200', cursor: 'pointer'}} color="white" mr={30} bg="transparent">
         <option value='option1'>Login into Business Account</option>
         <option value='option2'>Claim Your Business</option>
         <option value='option3'>Explore Yelp For Business</option>
       </Select>
       <Button colorScheme='blue' _hover={{ bg: 'blue.600', cursor: "pointer" }} mr={10}>Write an Article </Button>
       <Button colorScheme='blue' _hover={{ bg: 'blue.600' }} mr={10} color="Blue">Login</Button>
-      <Button colorScheme='blue' _hover={{ bg: 'Green.600' }} mr={10} color="Blue">Register</Button>
-    </Flex>  
+      <Button colorScheme='blue' _hover={{ bg: 'red.500' }} mr={10} color="Blue">Register</Button>
+    </Flex>    
             
 
         </div>
